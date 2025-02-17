f63da284ae17d926cbcdb63b594a4c3e8436bab1
Dragonlord8781 <evan.stone638@gmail.com>
2025-02-17 10:45:38 -0600

    Save

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, User!");
+Console.WriteLine("Please insert first name");
+string input1 = Console.ReadLine();
+Console.WriteLine($"Hello {input1}, please specify with last name");
+string input2 = Console.ReadLine();
+Console.WriteLine($"Thank you, Hello {input1} {input2}");
